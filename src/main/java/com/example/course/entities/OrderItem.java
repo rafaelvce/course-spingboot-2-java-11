40f20792b1a75876b56b848c85933b7e300951e1
Rafael Ceconi <rafaelvce@gmail.com>
2020-06-12 17:05:20 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable{
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	public double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
